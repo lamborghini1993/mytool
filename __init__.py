cce0eb3a8d643cd7d9b43f8f0ba716ce8f5e0b85
lamborghini1993 <1323242382@qq.com>
2017-11-01 16:11:54 +0800

    first blud

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: lamborghini1993
+@Date: 2017-11-01 15:07:48
+@Last Modified by:   lamborghini1993
+@Last Modified time: 2017-11-01 15:07:48
+@Desc:  一些公共函数的定义
+"""
