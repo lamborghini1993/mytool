2639b009d1b20d193cb736f2370e3781e69c73af
lamborghini1993 <1323242382@qq.com>
2017-11-12 01:57:47 +0800

    增加公共函数

--- pubdefines.py
+++ pubdefines.py
@@ -9,6 +9,7 @@
 
 import os
 import codecs
+import time
 
 
 def getpwd():
@@ -20,7 +21,51 @@ def write_to_file(filename, msg):
     pathname = os.path.join(getpwd(), filename)
     pathname += ".txt"
     dirname = os.path.dirname(pathname)
+    makedirs(dirname)
+    msg = "[{}]{}\n".format(time_to_str(), msg)
+    with codecs.open(pathname, "a", "utf-8") as myfile:
+        myfile.write(msg)
+
+
+def filter(msg):
+    msg = msg.replace("\r", "")
+    msg = msg.replace("\n", "")
+    return msg
+
+
+def makedirs(dirname):
     if not os.path.exists(dirname):
         os.makedirs(dirname)
-    with codecs.open(pathname, "a", "utf-8") as myfile:
-        myfile.write(msg + "\t\n")
+
+
+def dowland_pic(dirpath, name, picdata):
+    makedirs(dirpath)
+    filename = os.path.join(dirpath, name)
+    filename = os.path.join(getpwd(), filename)
+    with open(filename, "wb") as fpic:
+        fpic.write(picdata)
+
+
+def get_run_time(func):
+    def wrapped_func(*args):
+        begintime = time.time()
+        result = func(*args)
+        endtime = time.time()
+        print("%s() %s" % (func.__name__, endtime - begintime))
+        return result
+    return wrapped_func
+
+
+def get_sencond():
+    curtime = int(time.time())
+    return curtime
+
+
+def time_to_str(ti=-1, timeformat="%Y-%m-%d %H:%M:%S"):
+    if ti < 0:
+        ltime = time.localtime()
+    else:
+        ltime = time.localtime(ti)
+    strtime = time.strftime(timeformat, ltime)
+    return strtime
+
