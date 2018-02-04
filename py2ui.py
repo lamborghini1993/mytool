9dfe1e605327abd563d3575b6f3546725b796779
lamborghini1993 <1323242382@qq.com>
2018-02-04 17:52:53 +0800

    添加vscode设置 py2ui

---
+++ py2ui.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+lstIgnore = [".git", ".vscode", "__pycache__", "mytool", "log"]
+
+def IsIgnore(sDir):
+    for sTmp in lstIgnore:
+        if sDir.find(sTmp) != -1:
+            return True
+    return False
+
+for sDir, lstDir, lstFile in os.walk(os.getcwd()):
+    if IsIgnore(sDir):
+        continue
+    for sFile in lstFile:
+        if not sFile.endswith(".ui"):
+            continue
+        sUIFile = os.path.join(sDir, sFile)
+        sPYFile = sUIFile[:-3] + "_ui.py"
+        os.system("pyuic5 -o %s %s" % (sPYFile, sUIFile))
+        print("ui2py:" + sPYFile)
