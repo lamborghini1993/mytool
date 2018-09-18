696b604d899410d7e446de79baf793f3157279c2
duoyi-window <1323242382@qq.com>
2018-09-18 14:21:33 +0800

    添加一些常用函数

--- trunk/misc.py
+++ trunk/misc.py
@@ -5,18 +5,20 @@
 @Desc: 各种通用函数
 """
 
-import json
 import os
+import sys
 import time
+import json
 import traceback
 import codecs
-import sys
+import logging
+
 
 def JsonDump(data, path, **myArgs):
     dJsonArgs = {
-        "ensure_ascii"  : False,
-        "allow_nan"     : False,
-        "indent"        : 4,
+        "ensure_ascii": False,
+        "allow_nan": False,
+        "indent": 4,
     }
     dJsonArgs.update(myArgs)
     coding = dJsonArgs.pop("encoding", "utf-8")
@@ -33,6 +35,11 @@ def JsonLoad(path, default=None, **myArgs):
     return default
 
 
+def GetSecond():
+    curtime = int(time.time())
+    return curtime
+
+
 def Time2Str(ti=-1, timeformat="%Y-%m-%d %H:%M:%S"):
     if ti < 0:
         ltime = time.localtime()
@@ -47,8 +54,8 @@ def TraceMsg(*args):
     txtlist = GetTraceText(*args)
     for txt in txtlist:
         print(txt)
-        
-        
+
+
 def GetTraceText(*args):
     """用于获得回溯栈信息文本"""
     txtlist = []
@@ -66,6 +73,7 @@ def GetTraceText(*args):
         txtlist.append(txt)
     return txtlist
 
+
 def PythonError(msg=""):
     sErrTrace = traceback.format_exc()
     tb = sys.exc_info()[-1]
@@ -76,6 +84,21 @@ def PythonError(msg=""):
     return sLog
 
 
+def SysExceptHook(type, value, tb):
+    """
+    对于unchecked 异常,其实也是提供钩子来帮助我们处理的
+    我们可以在钩子里面记录崩溃栈追踪或者发送崩溃数据
+    重定向 sys.excepthook = SysExceptHook
+    """
+    msg = "".join(traceback.format_tb(tb))
+    value = str(value)
+    while tb.tb_next is not None:
+        tb = tb.tb_next
+    info = tb.tb_frame.f_locals
+    result = msg + value + "\n" + info
+    print(result)
+
+
 def Write2File(filename, msg, sType="a+"):
     filename += ".log"
     msg = "[{}]{}\n".format(Time2Str(), msg)
@@ -83,6 +106,36 @@ def Write2File(filename, msg, sType="a+"):
         myfile.write(msg)
 
 
-def GetSecond():
-    curtime = int(time.time())
-    return curtime
+def GetRunTime(func):
+    """获取func运行时间"""
+    def WrappedFunc(*args):
+        begintime = time.time()
+        result = func(*args)
+        endtime = time.time()
+        print("%s() %s" % (func.__name__, endtime - begintime))
+        return result
+    return WrappedFunc
+
+
+def InitLogging(sFileName):
+    """这里默认编码为gbk，暂时不知道如何修改"""
+    logging.basicConfig(
+        filename=sFileName + ".log",
+        format="[%(asctime)s %(levelname)s %(message)s]",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.DEBUG,
+    )
+    ch = logging.StreamHandler()
+    logger = logging.getLogger()
+    logger.addHandler(ch)
+
+
+def InitLogging2(sFileName):
+    handler = logging.FileHandler(filename=sFileName, mode="a", encoding="utf-8")
+    handler.setFormatter(logging.Formatter("[%(asctime)s %(levelname)s %(message)s]"))
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    logger.addHandler(ch)
